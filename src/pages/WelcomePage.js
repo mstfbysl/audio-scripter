4c11810406fdf6f29092d66fc6c790b27776e305
Mustafa Baysal <webmbaysal@icloud.com>
2024-02-04 17:20:44 +0100

    Added API key mandatory.

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Flex,
   Textarea,
+  Text,
+  Button,
   Tabs,
   TabList,
   Tab,
@@ -12,17 +14,24 @@ import {
   Container,
   Heading
 } from '@chakra-ui/react';
-import languages from '../content/languages.json';
 
 import UploadTab from '../components/UploadTab';
 import RecordTab from '../components/RecordTab';
 import WelcomeMessage from '../components/WelcomeMessage';
-import { useNotes } from '../context/NotesContext'; // Import the hook
+import { useNotes } from '../context/NotesContext'; 
 
 function WelcomePage() {
   const [selectedLanguage, setSelectedLanguage] = useState('');
   const [tabIndex, setTabIndex] = useState(0);
-  const { notes, handleNotesChange } = useNotes(); // Use the hook
+  const { notes, handleNotesChange } = useNotes();
+  const [isApiDisabled, setIsApiDisabled] = useState(false);
+
+  useEffect(() => {
+    const apiKey = localStorage.getItem('apiKey');
+    if (!apiKey || !apiKey.startsWith('sk')) {
+      setIsApiDisabled(true);
+    }
+  }, []);
 
   const handleLanguageChange = (event) => {
     setSelectedLanguage(event.target.value);
@@ -32,12 +41,27 @@ function WelcomePage() {
     setTabIndex(index);
   };
 
+  const goToSettings = () => {
+    window.location.href = '/settings';
+  };
+
+  if (isApiDisabled) {
+    return (
+      <Container maxW="8xl" centerContent>
+        <Heading mt="6" mb="4">API Key Required</Heading>
+        <Text>Please go to the settings page to enter your API Key.</Text>
+        <Button mt="4" colorScheme="teal" onClick={goToSettings}>
+          Go to Settings
+        </Button>
+      </Container>
+    );
+  }
+
   return (
     <Container maxW="8xl">
       <Flex direction={{ base: 'column', lg: 'row' }} gap={6} mt={4}>
         <Box flex="1" p={4}>
             <WelcomeMessage />
-
             <Tabs onChange={handleTabChange} index={tabIndex} isFitted >
                 <TabList mb="1em">
                   <Tab>Record your voice</Tab>
