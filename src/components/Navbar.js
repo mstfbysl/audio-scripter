9b570495553174f271fea286d12e37bb74a749ca
Mustafa Baysal <webmbaysal@icloud.com>
2024-04-26 16:32:37 +0300

    Buy me a coffee added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,11 +11,16 @@ import {
 } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
-import { FiHome, FiSettings } from 'react-icons/fi';
+import { FiHome, FiSettings, FiCoffee } from 'react-icons/fi';
 
 import logo from '../images/logo.png';
 
 function Navbar() {
+
+  const buyMeACoffee = () => {
+    window.open('https://buymeacoffee.com/mbaysal', '_blank');
+  };
+
   return (
     <Box boxShadow="md" p={4}>
       <Flex align="center">
@@ -26,6 +31,9 @@ function Navbar() {
         </Button>
         <Button as={ReactRouterLink} to="/settings" variant="ghost" mr={2} leftIcon={<Icon as={FiSettings} />}>
           Settings
+        </Button>
+        <Button onClick={buyMeACoffee} variant="ghost" mr={2} leftIcon={<Icon as={FiCoffee} />}>
+          Buy me a coffee
         </Button>
         <ColorModeSwitcher justifySelf="flex-end" />
       </Flex>
